6aa561c7d756703a44f150f17059e50dc2f69297
lecyp12 <contact@cyprien-cotinaut.com>
2023-03-26 12:57:40 +0200

    correctif builder

--- lib/screen/market_screen.dart
+++ lib/screen/market_screen.dart
@@ -46,30 +46,37 @@ class _FruitMasterState extends State<FruitMaster> {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           Provider.of<FruitsModel>(context, listen: false).set(snapshot.data);
-          return Scaffold(
-            appBar: AppBar(
+          return Consumer<FruitsModel>(
+            builder: (context, model, child) => Scaffold(
+              appBar: AppBar(
                 title: Consumer<CartModel>(builder: (context, model, child) {
-              return Text('Total panier ${model.totalPriceFormat()}');
-            })),
-            body: Column(children: [
-              Row(children: const [
-                SeasonFilter(),
-              ]),
-              Flexible(child:
-                  Consumer<FruitsModel>(builder: (context, model, child) {
-                // return Text(cart.seasonSelected);
-                return ListView(
-                  children: model.fruits
-                      .map((fruit) => FruitPreview(fruit: fruit))
-                      .toList(),
-                );
-              }))
-            ]),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const CartState())),
-              tooltip: 'Nouveau fruit',
-              child: const Icon(Icons.shopping_bag_outlined),
+                  return Text('Total panier ${model.totalPriceFormat()}');
+                }),
+              ),
+              body: Column(
+                children: [
+                  Row(
+                    children: const [
+                      SeasonFilter(),
+                    ],
+                  ),
+                  Flexible(
+                    child: ListView(
+                      children: model.fruits
+                          .map((fruit) => FruitPreview(fruit: fruit))
+                          .toList(),
+                    ),
+                  ),
+                ],
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const CartState()),
+                ),
+                tooltip: 'Nouveau fruit',
+                child: const Icon(Icons.shopping_bag_outlined),
+              ),
             ),
           );
         } else if (snapshot.hasError) {
